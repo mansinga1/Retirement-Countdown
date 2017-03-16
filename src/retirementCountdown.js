e2607df5797420ed53a09d1efebbb045ef5a639b
notpace <notpace@gmail.com>
2017-03-15 23:35:19 -0400

    Remove unnecessary comment

--- src/retirementCountdown.js
+++ src/retirementCountdown.js
@@ -6,12 +6,6 @@ var AlexaSkill = require('./AlexaSkill'),
 var APP_ID = "amzn1.ask.skill.460c7ead-9f94-41e7-b574-61de5d837927";
 var skillContext = {};
 
-/**
- * ScoreKeeper is a child of AlexaSkill.
- * To read more about inheritance in JavaScript, see the link below.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
- */
 var RetirementCountdown = function () {
     AlexaSkill.call(this, APP_ID);
     skillContext.needMoreHelp = true;
